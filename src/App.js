c74acf3e327fc0d961bc42b477dd91477fa36a91
Mohammad Althayabeh <althayabeh.mohammad@gmail.com>
2018-05-26 17:19:25 -0400

    create static ContactList component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,38 @@
 import React, { Component } from 'react';
 
+
+class ContactList extends Component{
+    render(){
+      return (
+        <div className="contact-list">
+          <div className="contact-list-item">
+            <div className="contact-avatar">
+              <img src=""/>
+            </div>
+            <div className="contact-details">
+              <p>Name</p>
+              <p>Email</p>
+            </div>
+          </div>
+          <div className="contact-list-item">
+            <div className="contact-avatar">
+              <img src=""/>
+            </div>
+            <div className="contact-details">
+              <p>Name</p>
+              <p>Email</p>
+            </div>
+          </div>
+        </div>
+      )
+    }
+}
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-
+        <ContactList />
       </div>
     );
   }
