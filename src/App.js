877a7636d9fad20b3fd3faac711d01f7d73a6a34
Mohammad Althayabeh <althayabeh.mohammad@gmail.com>
2018-05-26 18:20:15 -0400

    add initial state of contacts

--- src/App.js
+++ src/App.js
@@ -1,32 +1,37 @@
 import React, { Component } from 'react'
 import ContactList from './ContactList'
 
-const contacts = [
-    {
-      id: 'ryan',
-      name: 'Ryan Florence',
-      email: 'ryan@reacttraining.com',
-      avatarURL: 'http://localhost:5001/ryan.jpg'
-    },
-    {
-      id: 'michael',
-      name: 'Michael Jackson',
-      email: 'michael@reacttraining.com',
-      avatarURL: 'http://localhost:5001/michael.jpg'
-    },
-    {
-      id: 'tyler',
-      name: 'Tyler McGinnis',
-      email: 'tyler@reacttraining.com',
-      avatarURL: 'http://localhost:5001/tyler.jpg'
-    }
-  ]
-
 class App extends Component {
+
+  constructor(props){
+    super(props)
+    this.state = {
+      contacts: [
+          {
+            id: 'ryan',
+            name: 'Ryan Florence',
+            email: 'ryan@reacttraining.com',
+            avatarURL: 'http://localhost:5001/ryan.jpg'
+          },
+          {
+            id: 'michael',
+            name: 'Michael Jackson',
+            email: 'michael@reacttraining.com',
+            avatarURL: 'http://localhost:5001/michael.jpg'
+          },
+          {
+            id: 'tyler',
+            name: 'Tyler McGinnis',
+            email: 'tyler@reacttraining.com',
+            avatarURL: 'http://localhost:5001/tyler.jpg'
+          }
+        ]
+    }
+  }
   render() {
     return (
       <div className="App">
-        <ContactList contacts={contacts} />
+        <ContactList contacts={this.state.contacts} />
       </div>
     );
   }
